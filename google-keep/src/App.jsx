126b5c0e87030a2fd51a57af3407e80f8d229d18
Lucas Motshophi <motshophi@gmail.com>
2025-05-08 01:10:34 +0200

    first commit

---
+++ google-keep/src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Form from './components/auth/Form';
+import MainUI from './components/ui/MainUI';
+
+const App = () => {
+  const [showMainUI, setShowMainUI] = useState(false);
+
+  return (
+    <div>
+      {showMainUI ? (
+        <MainUI />
+      ) : (
+        <Form onLogin={() => setShowMainUI(true)} />
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ google-keep/src/components/ui/MainUI.jsx
@@ -0,0 +1,10 @@
+const MainUI = () => {
+    return (
+        <div>
+            <h1>Welcome to the Main UI!</h1>
+
+        </div>
+    );
+};
+
+export default MainUI;
